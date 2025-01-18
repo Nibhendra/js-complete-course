8f789f68641466c54741278aa9be9b3039f00065
Nibhendra Deo <nibhendrad@gmail.com>
2025-01-18 05:24:12 +0000

    variables and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 112233
+let accountEmail = "nibhendra@google.com"
+var accountpassword = "12345"
+accountCity = "Delhi"
+let accountState;
+// accountId = 2 // not allowed
+
+
+accountEmail = "nd@nd.com"
+accountpassword = "121212"
+accountCity = "Bombay"
+
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+
+/*
+prefer not to use  var
+bracuse of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3 )  // we are nodejs, not browser
+
+console.log(3+3)
+console.log("Nid")
+
+let name = "nibhendra"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => when the variable is declared but the value is not assigned
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
